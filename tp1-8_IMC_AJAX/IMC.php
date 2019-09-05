ece0bb09d969e2a9715fef73f351ef92bdb3c93c
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-09-05 13:53:35 -0300

    TP1-8 IMC con Ajax - Solved

---
+++ tp1-8_IMC_AJAX/IMC.php
@@ -0,0 +1,18 @@
+<?php
+    //echo $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']);
+    if (isset($_GET['peso']) && isset($_GET['altura']))   {
+        $peso = $_GET['peso'];
+        $altura = $_GET['altura'];
+        $IMC = $peso / ($altura*$altura);
+        //echo $IMC;
+        if ($IMC < 18.5) {
+            echo "<p> Bajo peso </p>";
+        } else if (($IMC >= 18.5) && ($IMC < 25) ) {
+            echo "<p> Normal </p>";
+        } else if (($IMC >= 25) && ($IMC < 30) ) {
+            echo "<p> Sobrepeso </p>";
+        } else if ($IMC >= 30) {
+            echo "<p> Obesidad </p>";
+        }
+    }
+?> 
