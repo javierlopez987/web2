ac44ed88c07bb052aab3d47467f56443dd1d084e
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-09-08 21:02:39 -0300

    Se le agrega Boostrap

--- presentismo_dev/presentismo.php
+++ presentismo_dev/presentismo.php
@@ -45,7 +45,12 @@
     function showPresentismo() {
         $objAsistencia = getPresentismo();
         //var_dump($objAsistencia);die;
-        $div = "<table>";
+        $div = "<table> <tr>
+            <th> Fecha </th> 
+            <th> Materia </th>
+            <th> Alumno </th> 
+            <th> Asistencia </th>
+        </tr>";
         foreach ($objAsistencia as $asistencia) {
                 if($asistencia->asistencia != 0) {
                     $asis = "checked";
@@ -74,7 +79,7 @@
                 <input type="text" name="denominacion" placeholder="Título">
                 <input type="text" name="docente" placeholder="Docente">
                 <input type="text" name="descripcion"  placeholder="Descripción">
-                <button type="submit">Insertar</button>
+                <button type="submit" class="btn btn-outline-primary">Insertar</button>
             </form>';
         return $div;
     }
@@ -93,7 +98,7 @@
                 <input type="date" name="f_nac">
                 <input type="email" name="email"  placeholder="ejemplo@gmail.com">
                 <input type="text" name="celular"  placeholder="+549 249 4490755">
-                <button type="submit">Insertar</button>
+                <button type="submit" class="btn btn-outline-primary">Insertar</button>
             </form>';
         return $div;
     }
@@ -117,13 +122,18 @@
         <html lang="en">
         <head>
             <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0", shrink-to-fit=no">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <title>Presentismo</title>
         </head>
         <body>';
         $html .= $contenido;
-        $html .='</body>
+        $html .='
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        </body>
         </html>';
 
         echo $html;
