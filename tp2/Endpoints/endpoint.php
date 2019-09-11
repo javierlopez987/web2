68ebfb62b9c66e145a364f876e94a74fe475182e
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-09-11 20:49:19 -0300

    se eliminó tag base que creaba conflicto

--- tp2/Endpoints/endpoint.php
+++ tp2/Endpoints/endpoint.php
@@ -122,7 +122,6 @@ define("BASE", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dir
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0", shrink-to-fit=no">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <base href='.BASE.' >
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <title>Presentismo</title>
         </head>
